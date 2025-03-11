67e5f748ed86e82f2e4585c5afb32d02d61d3f28
Muaz-rana95 <rana_muaz@hotmail.co.uk>
2025-03-11 13:55:57 +0000

    changes made again

--- hive_etl2.py
+++ hive_etl2.py
@@ -1,19 +1,21 @@
 from pyspark.sql import *
 from pyspark.sql.functions import *
-from pyspark.sql.functions import col, upper, monotonically_increasing_id
+import pyspark.sql.functions as F
+from pyspark.sql.functions import col, upper
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.master("local").appName("TfL Underground ETL").enableHiveSupport().getOrCreate()
+try:
+    df = spark.sql("SELECT * FROM default.tfl_undergroundrecord")
+    print("data successfully read")
+except Exception as e:
+    print("Error occurred during Hive operations")
 
-df = spark.sql("SELECT * FROM default.tfl_undergroundrecord")
-print("data successfully read")
-
-
-df_transformed = df.withColumn("status", upper(col("status"))) \
-    .withColumn("record_id", monotonically_increasing_id() + 1)
+# Example Transformation: Convert all status descriptions to uppercase
+df_transformed = df.withColumn("status", upper(col("status")))
 
 try:
-    df_transformed.write.mode("overwrite").saveAsTable("default.tfl_undergroundresult")
+    df_transformed.write.mode("overwrite").saveAsTable("default.tfl_undergroundresul")
     print("Successfully Load to Hive")
 except Exception as e:
     print("Error occurred during Hive operations")
